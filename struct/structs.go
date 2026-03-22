cd3566afdbfbda716377e0ce896ebfa89ef370e1
Abhipatil848 <abhishekpatil7513@gmail.com>
2026-03-22 23:38:29 +0530

    structs

---
+++ struct/structs.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+)
+
+// define service struct
+type Service struct {
+	Name    string
+	Port    int
+	Healthy bool
+}
+
+func printStatus(s Service) {
+	status := "Healthy"
+	if !s.Healthy {
+		status = "Unhealthy"
+	}
+
+	msg := fmt.Sprintf("Name: %s | Port: %d | %s", s.Name, s.Port, status)
+
+	if s.Healthy {
+		color.HiGreen(msg)
+	} else {
+		color.Red(msg)
+	}
+}
+
+func main() {
+	fmt.Println("hello, structs")
+
+	services := []Service{
+		{Name: "gateway", Port: 8080, Healthy: true},
+		{Name: "postgres", Port: 5432, Healthy: false},
+		{Name: "frontend", Port: 443, Healthy: true},
+	}
+
+	for _, svc := range services {
+		printStatus(svc)
+	}
+}
